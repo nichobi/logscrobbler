e65ee357393eeb3773f4191c38d333e575c14f93
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2023-09-15 15:00:42 +0200

    Swap variables for temporary files for payload

--- logscrobbler.sh
+++ logscrobbler.sh
@@ -9,22 +9,22 @@ is_valid_mbid() {
    echo "$1" | grep -P '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}' > /dev/null
 }
 
-auth_token=""
+auth_token="f0354180-a7e2-4c7b-a86a-ac953038e223"
 client=$(head "$1" | grep -Po '#CLIENT/\K.*')
 timezone=$(head "$1" | grep -Po '#TZ/\K.*')
 tz_offset=$(date +%:z | awk '{print substr($1, 1, 1) substr($1, 2, 2)*60*60} + substr($1, 4, 2) * 60')
 
 submit() {
-  #echo "$JSON" | jq
   headerfile=$(mktemp)
+  payload_file_compact=$(mktemp)
+  jq . --compact-output "$payload_file" > "$payload_file_compact"
   if curl 'https://api.listenbrainz.org/1/submit-listens' \
     --header "Authorization: Token $auth_token" \
     --fail-with-body -D "$headerfile" \
-    --json "$(echo "$JSON" | jq --compact-output)"
+    --json "@$payload_file_compact"
   then
     echo "Successfully submited $payload_listens listens"
     remaining=$(grep -oP "x-ratelimit-remaining: \K\d+" "$headerfile")
-    echo remaining="$remaining"
     if [ "$remaining" -lt 1 ]; then
       resetin=$(grep -oP "x-ratelimit-reset-in: \K\d+" "$headerfile")
       echo sleeping "$resetin"
@@ -32,7 +32,7 @@ submit() {
     fi
   else
     echo "Failed to submit $payload_listens listens"
-    echo "$JSON" >> "$1".failed
+    mv "$payload_file_compact" "$1".failed
   fi
   rm "$headerfile"
   payload_listens=0
@@ -56,15 +56,15 @@ while IFS= read -r line
 
     [ "$timezone" = 'UNKNOWN' ] && timestamp=$((timestamp - tz_offset))
     if [ "$payload_listens" -lt 1 ]; then
-      JSON='
-      {
+      payload_file=$(mktemp)
+      echo '{
         "listen_type": "import",
-        "payload": ['
+        "payload": [' > "$payload_file"
         payload_listens=0
     else
-      JSON="$JSON"','
+      echo "," >> "$payload_file"
     fi
-    JSON="$JSON$(jq -n \
+    jq -n \
       '{
          "listened_at": $timestamp,
          "track_metadata": {
@@ -86,21 +86,17 @@ while IFS= read -r line
       --arg     client    "$client"       \
       --arg     mbid      "$mbid"         \
       --argjson tracknr   "${tracknr:-0}" \
-      --argjson duration  "$duration"
+      --argjson duration  "$duration"     \
+    >> "$payload_file"
       # tracknr has a default so jq won't complain if it's missing
-    )"
     payload_listens=$((payload_listens + 1))
     if [ "$payload_listens" -eq 1000 ]; then
-      JSON="$JSON"'
-        ]
-      }'
+      echo ']}' >> "$payload_file"
       submit "$@"
     fi
   done <"$1"
 if [ "$payload_listens" -ge 1 ]; then
-      JSON="$JSON"'
-        ]
-      }'
+  echo ']}' >> "$payload_file"
   submit "$@"
 fi
 
